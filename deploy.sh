29fe467e6cfb9ce6acb298d3a55ffe3ca29278fe
MuggleNet <98475113+MuggleNet@users.noreply.github.com>
2022-01-26 13:24:24 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MuggleNet/vue-wordle.git master:gh-pages
+git push -f git@github.com:mugglenet/vue-wordle.git master:gh-pages
 
 cd -
